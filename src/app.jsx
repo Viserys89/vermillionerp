9b6216bb275fa6913b41b167dabe5fac3999040a
Viserys89 <fistvicky@gmail.com>
2026-04-09 20:08:56 +0700

    feat: add Procurement Layout

--- src/app.jsx
+++ src/app.jsx
@@ -1,20 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-import ContactPage from './pages/ContactPage';
-import MainLayout from './components/layout/MainLayout';
-import BerandaHost from './pages/host/BerandaHost';
-import LaporanHost from './pages/host/LaporanHost';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import ContactPage from "./pages/ContactPage";
+import MainLayout from "./components/layout/MainLayout";
+import BerandaHost from "./pages/host/BerandaHost";
+import LaporanHost from "./pages/host/LaporanHost";
+import BerandaProcurement from "./pages/procurement/BerandaProcurement"; 
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Halaman tanpa Sidebar */}
         <Route path="/" element={<LoginPage />} />
         <Route path="/contact" element={<ContactPage />} />
-
+        {/* Dashboard Role: HOST */}
         <Route path="/dashboard-host" element={<MainLayout />}>
-          <Route index element={<BerandaHost />} /> 
-          <Route path="laporan" element={<LaporanHost />} /> 
+          <Route index element={<BerandaHost />} />
+          <Route path="laporan" element={<LaporanHost />} />
+        </Route>
+        {/* Dashboard Role: PROCUREMENT */}
+        <Route path="/dashboard-procurement" element={<MainLayout />}>
+          <Route index element={<BerandaProcurement />} />
         </Route>
       </Routes>
     </Router>
